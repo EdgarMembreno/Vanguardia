7a25a761a0f5fe9362056fbf41b09e4acf049977
EdgarMembreno <edgar.membreno2014.com>
2024-04-25 19:11:45 -0600

    Se anadieron las nuevas rutas

--- Sistema de Inventario/app.js
+++ Sistema de Inventario/app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
 app.get('/', (req, res) => {
+    res.send('¡Bienvenido al Sistema de Inventario!');
+});
+
+app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+    console.log(`El servidor está escuchando en http://localhost:${port}`);
 });
