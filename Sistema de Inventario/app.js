da76615093a2647e6a260643c2ac96a7dee19f5e
EdgarMembreno <edgar.membreno2014.com>
2024-04-25 19:05:47 -0600

    Se anadio Express Hello World

--- Sistema de Inventario/app.js
+++ Sistema de Inventario/app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('¡Bienvenido al Sistema de !');
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-    console.log(`El servidor está escuchando en http://localhost:${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
